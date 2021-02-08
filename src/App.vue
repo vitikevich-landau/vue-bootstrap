5090f252f2a4ab1eea9d6be41e21e480defd2d37
Viktor <vitikevich@gmail.com>
2021-02-09 00:15:41 +0700

    # init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar class="mb-4" type="dark" variant="primary">
+      <b-navbar-brand href="#">FORMS.PARUSALTAI.RU</b-navbar-brand>
+
+<!--      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>-->
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-form-checkbox button-variant="outline-secondary" switch size="lg"></b-form-checkbox>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <b-container class="justify-content-md-center">
+      <b-row>
+        <b-col cols="12">
+          <SimpleForm></SimpleForm>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import SimpleForm from "@/components/SimpleForm";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      SimpleForm
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    color: #2c3e50;
+  }
+
+  .justify-content-md-center {
+    max-width: 576px !important;
+  }
+
+  .custom-control-input:checked ~ .custom-control-label::before {
+    background-color: #28a745 !important;
+  }
+  .navbar-brand {
+    font-size: 1rem !important;
+  }
 </style>
