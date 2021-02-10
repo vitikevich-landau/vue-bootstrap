a61e86bc08ecf0ecd687e63334340b0a87f03f68
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-02-10 12:43:10 +0700

    # Отмена выделения номера телефона при клике на switch

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,9 +2,9 @@
   <b-navbar class="mb-4" type="dark" variant="primary">
     <img src="../assets/logo_parus1.png" alt="">
     <b-navbar-brand class="ml-2">FORMS.PARUSALTAI.RU</b-navbar-brand>
-        <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="ml-auto" >
-        <b-form-checkbox id="www"  button-variant="outline-secondary" switch size="lg"></b-form-checkbox>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-form-checkbox id="www" button-variant="outline-secondary" switch size="lg"></b-form-checkbox>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -21,4 +21,8 @@
     font-size: 1rem !important;
   }
 
+  .custom-switch.b-custom-control-lg,
+  .custom-control-label::before {
+    user-select: none !important;
+  }
 </style>
