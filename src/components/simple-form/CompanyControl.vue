74ded32d582242c7fb20f4aeb0760859c8556887
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-02-11 11:44:28 +0700

    # Add CompanyControl.vue

---
+++ src/components/simple-form/CompanyControl.vue
@@ -0,0 +1,58 @@
+<template>
+  <!--  Company  -->
+  <b-form-group
+      v-if="verified"
+      id="company_group"
+      :disabled="sending"
+      label-class="font-weight-bold"
+  >
+    <template v-slot:label>
+      Организация:
+    </template>
+    <b-form-select
+        id="company"
+        v-model="company_"
+        :options="companies"
+        :state="success"
+    ></b-form-select>
+  </b-form-group>
+</template>
+
+<script>
+  import {store} from '../../store';
+  import {mapGetters, mapMutations} from 'vuex';
+
+  export default {
+    name: "CompanyControl",
+    store,
+    data() {
+      return {}
+    },
+    computed: {
+      ...mapGetters([
+        'verified',
+        'sending',
+        'companies',
+        'company',
+        'success'
+      ]),
+      company_: {
+        get () {
+          return this.company;
+        },
+        set(v) {
+          this.setCompany(v);
+        },
+      }
+    },
+    methods: {
+      ...mapMutations([
+        'setCompany'
+      ])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
