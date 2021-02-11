db960c6874e0d5e50f262a376a5d51360ec26da3
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-02-11 10:56:40 +0700

    # Add PhoneControl.vue

---
+++ src/components/simple-form/PhoneControl.vue
@@ -0,0 +1,176 @@
+<template>
+  <!--  Phone  -->
+  <b-form-group
+      id="phone_group"
+      label-class="font-weight-bold"
+      :disabled="sending"
+  >
+    <template v-slot:label>
+      Номер телефона
+      <span :style="{fontSize: '18px'}" class="text-danger">
+            <strong>*</strong>
+          </span>
+    </template>
+    <b-input-group>
+      <!-- Show while data load -->
+      <template #append v-if="verifying">
+        <b-input-group-text>
+          <b-icon-arrow-repeat animation="spin"></b-icon-arrow-repeat>
+        </b-input-group-text>
+      </template>
+
+      <b-form-input
+          id="phone"
+          @input="onInputPhone"
+          :value="phone"
+          type="text"
+          autocomplete="off"
+          placeholder="+7 (___) ___ ____"
+          v-mask="'+7 (###) ### ####'"
+          :state="success"
+      ></b-form-input>
+    </b-input-group>
+  </b-form-group>
+</template>
+
+<script>
+  import {mask} from 'vue-the-mask';
+  import {BIconArrowRepeat} from 'bootstrap-vue';
+  import {store} from '../../store';
+  import {mapGetters, mapMutations, mapActions} from 'vuex';
+
+  export default {
+    name: "PhoneControl",
+    store,
+    directives: {mask},
+    components: {
+      BIconArrowRepeat
+    },
+    data() {
+      return {
+        previousPhone: null,
+        /***
+         *  Request cancel
+         * */
+        controller: null,
+        /***
+         *  Show input preloader
+         * */
+        verifying: false
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'phone',
+        'phoneFilled',
+        'formCompleted',
+        'success',
+        'sending',
+        'verified'
+      ])
+    },
+    methods: {
+      ...mapMutations([
+        'setPhone',
+        'setPhoneFilled',
+        'setSuccess',
+        'setCompany',
+        'setName'
+      ]),
+      ...mapActions([
+        'verifyUser'
+      ]),
+      onInputPhone(v) {
+        this.setPhone(v);
+
+        /***
+         *
+         *   Последний символ в запросе должен быть цифрой
+         * */
+        if (v.length === 17 && /[0-9]/.test(v[v.length - 1])) {
+
+
+          /***
+           *  Если телефон заполнен и предыдущее значение не равно текущему
+           * */
+          if (!this.phoneFilled || this.previousPhone !== v) {
+            this.previousPhone = v;
+
+            // console.log(`v: ${v}, previousPhone: ${this.previousPhone}`);
+
+            this.verifyPhone(v);
+          }
+
+          this.setPhoneFilled(true);
+        } else if (v.length < 17) {
+
+          this.setPhoneFilled(false);
+          this.setSuccess(null);
+        }
+
+        console.log(v, v.length, this.phoneFilled);
+
+      },
+      async verifyPhone(phone) {
+        /***
+         *  Cancel previous request
+         * */
+        if (this.controller) {
+          this.controller.abort();
+        }
+
+        /***
+         *  Send request to server
+         */
+        console.log('send request to server...');
+
+        this.controller = new AbortController();
+        const {signal} = this.controller;
+
+        try {
+          /***
+           *  Before fetch
+           * */
+          this.verifying = true;
+          this.setSuccess(null);
+
+
+          await this.verifyUser({signal, phone});
+
+          /***
+           *  Зыакрываем, если были открыты
+           * */
+          this.$bvToast.hide();
+
+          if(this.verified) {
+            this.setSuccess(true);
+          } else {
+            this.setSuccess(null);
+          }
+
+        } catch (e) {
+          if (e.name === 'AbortError') {
+            console.log('Request cancelled...');
+          } else {
+            /***
+             *  Зыакрываем, если были открыты
+             * */
+            this.$bvToast.hide();
+
+            this.$emit('showErrorToast');
+            this.setSuccess(false);
+          }
+
+        } finally {
+          console.log('finally');
+
+          this.verifying = false;
+        }
+      },
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
