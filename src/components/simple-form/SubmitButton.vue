a738533b527d5c13b19ae15c7627510799d703e4
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-02-11 14:59:01 +0700

    # Add SubmitButton.vue

---
+++ src/components/simple-form/SubmitButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-button
+        v-if="!sending"
+        type="submit"
+        variant="primary"
+        :disabled="!formCompleted"
+    >
+      Отправить
+    </b-button>
+    <b-button
+        v-else
+        variant="primary"
+        type="button"
+        disabled>
+      <b-spinner small type="grow"></b-spinner>
+      Отправка...
+    </b-button>
+  </div>
+</template>
+
+<script>
+  import {store} from '../../store';
+  import {mapGetters} from 'vuex';
+
+  export default {
+    name: "SubmitButton",
+    store,
+    data() {
+      return {}
+    },
+    computed: {
+      ...mapGetters([
+        'sending',
+        'formCompleted'
+      ])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
