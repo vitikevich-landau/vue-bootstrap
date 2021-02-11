293ae9bf2f1b67856a0e77087a48fb087a62afca
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-02-11 15:12:16 +0700

    # Add SimpleForm.vue

---
+++ src/components/simple-form/SimpleForm.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <b-form @submit.prevent="onSubmit">
+      <PhoneControl
+          @hideToasts="hideToasts"
+          @showErrorToast="showErrorToast"/>
+      <CompanyControl/>
+      <NameControl/>
+      <MessageControl/>
+      <SubmitButton/>
+    </b-form>
+
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ formData }}</pre>
+      <!--          <pre class="m-0">{{ verifyingData }}</pre>-->
+    </b-card>
+  </div>
+</template>
+
+<script>
+  import delay from "../../lib/delay";
+  import {store} from '../../store/index';
+  import {mapGetters, mapMutations} from 'vuex';
+  import PhoneControl from "./PhoneControl";
+  import CompanyControl from "./CompanyControl";
+  import NameControl from "./NameControl";
+  import MessageControl from "./MessageControl";
+  import SubmitButton from "./SubmitButton";
+
+  export default {
+    name: "SimpleForm",
+    components: {
+      PhoneControl,
+      CompanyControl,
+      NameControl,
+      MessageControl,
+      SubmitButton
+    },
+    store,
+    data() {
+      return {
+        sending: false,
+      }
+    },
+    computed: {
+      /***
+       *  Dedug
+       * */
+      ...mapGetters([
+        'formData',
+        'verifyingData'
+      ]),
+    },
+    methods: {
+      ...mapMutations([
+        'resetForm',
+        'setSuccess',
+        'setSending'
+      ]),
+      hideToasts() {
+        this.$bvToast.hide();
+      },
+      showSuccessToast(/*msg*/) {
+        this.makeToast('success', 'Успех!', 'Ваше сообщение отправлено...');
+      },
+      showErrorToast(/*msg*/) {
+        this.makeToast('danger', 'Ошибка!', 'Ошибка соединения с сервером... Попробуйте чуть позже');
+      },
+      makeToast(variant, title, message) {
+        this.$bvToast.toast(message, {
+          toaster: 'b-toaster-top-center',
+          title,
+          variant: variant,
+          solid: true,
+          autoHideDelay: 17000
+        });
+      },
+      async onSubmit() {
+        /***
+         *  before send
+         * */
+        this.setSending(true);
+
+        try {
+
+          /***
+           *  Send post request to server
+           * */
+          const {data} = await delay();
+
+          /***
+           *  After send
+           * */
+          console.log(data);
+
+          /***
+           *  Зыакрываем, если были открыты
+           * */
+          this.$bvToast.hide();
+          this.showSuccessToast();
+
+          this.resetForm();
+
+          this.setSuccess(null);
+        } catch (e) {
+
+          /***
+           *  Зыакрываем, если были открыты
+           * */
+          this.$bvToast.hide();
+          this.showErrorToast();
+
+          this.setSuccess(false);
+        } finally {
+
+          this.setSending(false);
+        }
+      },
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
