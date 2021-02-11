2f893b09b7f1a9b0da1a39ab8aa64cccdf0ca671
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2021-02-11 11:23:03 +0700

    # Add Vuex

---
+++ src/store/index.js
@@ -0,0 +1,94 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    /***
+     *  Data to send to the server
+     * */
+    form: {
+      phone: '',
+      company: '',
+      name: '',
+      message: ''
+    },
+    /***
+     *  Данные о пользователе, после прохождения верификации
+     * */
+    verifyingData: [],
+    /***
+     *  Индикатор заполнения номера телефона
+     * */
+    phoneFilled: false,
+    /***
+     *  Для подсветки валидации
+     *  Используется всеми компонентами
+     * */
+    success: null,
+    /***
+     *  Show button preloader
+     *  Disable inputs while sending
+     * */
+    sending: false
+  },
+  getters: {
+    formData: state => state.form,
+    phone: state => state.phone,
+    company: state => state.company,
+    name: state => state.name,
+    message: state => state.message,
+    success: state => state.success,
+    sending: state => state.sending,
+    verifyingData: state => state.verifyingData,
+    verified: state => !!state.verifyingData.length,
+    phoneFilled: state => state.phoneFilled,
+    formCompleted: state => state.phoneFilled
+      && state.name.length > 1
+      && state.message.length > 1,
+    companies: state => state.verifyingData.map(v => v.title),
+    names: state => [...new Set(state.verifyingData.map(v => v.name))],
+  },
+  mutations: {
+    setPhone: (state, phone) => state.form.phone = phone,
+    setCompany: (state, company) => state.form.company = company,
+    setName: (state, name) => state.form.name = name,
+    setMessage: (state, message) => state.form.message = message,
+    setVerifyingData: (state, data) => state.verifyingData = data,
+    setPhoneFilled: (state, flag) => state.phoneFilled = flag,
+    setSuccess: (state, flag) => state.success = flag,
+    setSending: (state, flag) => state.sending = flag,
+    resetForm: state => {
+      state.form.phone = '';
+      state.form.company = '';
+      state.form.name = '';
+      state.form.message = '';
+
+      state.success = null;
+
+      state.verifyingData = [];
+    }
+  },
+  actions: {
+    verifyUser: async (
+      {commit, getters},
+      {signal, phone}
+    ) => {
+      const response = await fetch(
+        `http://192.168.1.132:8185/api/verify-phone?phone=${encodeURIComponent(phone)}`,
+        {signal}
+      );
+      const data = await response.json();
+
+      data.length
+        ? commit('setVerifyingData', data)
+        : commit('setVerifyingData', []);
+      /***
+       *  Присваиваем значение имя/организации, первым значением из списка
+       * */
+      commit('setCompany', getters.companies[0] ?? '');
+      commit('setName', getters.names[0] ?? '');
+    }
+  },
+});
